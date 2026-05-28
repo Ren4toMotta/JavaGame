1fd6ba9ffe6ca18b2b819caca6d2aead835b245a
Ren4toMotta <renato.motta2005@gmail.com>
2026-05-27 21:07:50 -0300

    first commit

---
+++ src/AULA/DESAFIO/Principal.java
@@ -0,0 +1,16 @@
+package AULA.DESAFIO;
+/*
+ * CAP10 -	GAMES2D EM JAVA
+ * 			CAP12 - MONTANDO O TABUTEILRO
+ * AUTOR: 	LUIZ CORCINI
+ * DATA: 	dd/mm/aaaa
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Cap12 - TABULEIRO");
+		new Moldura();
+
+	}
+
+}
