c5563a397315673e8333403c976b1da4d760f8a0
GustaGabe <notscry@outlook.com>
2026-06-20 16:36:26 -0300

    feat: new sprites

--- src/AULA/DESAFIO/Tiro.java
+++ src/AULA/DESAFIO/Tiro.java
@@ -1,19 +1,33 @@
 package AULA.DESAFIO;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.ImageIcon;
 
 public class Tiro {
 	public static final int RAIO = 6;
 	public static final double VELOCIDADE = 9.0;
+	private static final int LARG_SPRITE = 32;
+	private static final int ALTU_SPRITE = 16;
+	private static final int TICKS_POR_FRAME = 3;
+
+	private static final Image[] frames = new Image[5];
+	private static boolean carregado = false;
 
 	public double posX, posY;
 	public double vx, vy;
 	public boolean ativo = true;
 	public Rectangle areaColisao;
 
+	private int frame = 0;
+	private int contadorFrame = 0;
+	private double angulo;
+
 	public Tiro(int x, int y, int dx, int dy) {
+		carregarSprites();
 		this.posX = x;
 		this.posY = y;
 		double mag = Math.sqrt(dx * dx + dy * dy);
@@ -25,7 +39,18 @@ public class Tiro {
 			this.vx = (dx / mag) * VELOCIDADE;
 			this.vy = (dy / mag) * VELOCIDADE;
 		}
+		this.angulo = Math.atan2(this.vy, this.vx);
 		this.areaColisao = new Rectangle((int)posX - RAIO, (int)posY - RAIO, RAIO * 2, RAIO * 2);
+	}
+
+	private static synchronized void carregarSprites() {
+		if (carregado) return;
+		String base = "res/PLAYERS/PLAYER_SHOOTING/Zombie-Tileset---_0";
+		String suf = "_Capa-";
+		for (int i = 0; i < 5; i++) {
+			frames[i] = new ImageIcon(base + (370 + i) + suf + (371 + i) + ".png").getImage();
+		}
+		carregado = true;
 	}
 
 	public void atualizar(tileMap cena) {
@@ -44,12 +69,24 @@ public class Tiro {
 		if (VerificadorDeColisao.colideComTileEm(this.areaColisao, cena)) {
 			this.ativo = false;
 		}
+
+		contadorFrame++;
+		if (contadorFrame >= TICKS_POR_FRAME) {
+			frame = (frame + 1) % frames.length;
+			contadorFrame = 0;
+		}
 	}
 
 	public void desenhar(Graphics2D d2) {
-		d2.setColor(Color.YELLOW);
-		d2.fillOval((int)posX - RAIO, (int)posY - RAIO, RAIO * 2, RAIO * 2);
-		d2.setColor(Color.ORANGE);
-		d2.drawOval((int)posX - RAIO, (int)posY - RAIO, RAIO * 2, RAIO * 2);
+		Image img = frames[frame];
+		if (img == null) return;
+		AffineTransform original = d2.getTransform();
+		AffineTransform tx = new AffineTransform();
+		tx.translate(posX, posY);
+		tx.rotate(angulo);
+		tx.translate(-LARG_SPRITE / 2.0, -ALTU_SPRITE / 2.0);
+		d2.setTransform(tx);
+		d2.drawImage(img, 0, 0, LARG_SPRITE, ALTU_SPRITE, null);
+		d2.setTransform(original);
 	}
 }
--- src/AULA/DESAFIO/Zumbi.java
+++ src/AULA/DESAFIO/Zumbi.java
@@ -2,22 +2,62 @@ package AULA.DESAFIO;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
 
 public class Zumbi {
 	public static final int LARG = 40, ALTU = 40;
 	public static final int VIDA_INICIAL = 2;
 	public static final int PASSO = 2;
+	private static final int TICKS_POR_FRAME = 10;
+	private static final int DURACAO_DANO = 15;
 
 	public int posX, posY;
 	public int vida = VIDA_INICIAL;
 	public boolean vivo = true;
 	public Rectangle areaColisao;
 
+	private static final Image[] sprUp = new Image[3];
+	private static final Image[] sprRight = new Image[3];
+	private static final Image[] sprDown = new Image[3];
+	private static final Image[] sprUpDmg = new Image[3];
+	private static final Image[] sprRightDmg = new Image[3];
+	private static final Image[] sprDownDmg = new Image[3];
+	private static boolean spritesCarregados = false;
+
+	private int frame = 0;
+	private int contadorFrame = 0;
+	private int tempoDano = 0;
+	private String direcao = "down";
+	private boolean olhandoEsquerda = false;
+
 	public Zumbi(int x, int y) {
 		this.posX = x;
 		this.posY = y;
 		this.areaColisao = new Rectangle(x, y, LARG, ALTU);
+		carregarSprites();
+	}
+
+	private static synchronized void carregarSprites() {
+		if (spritesCarregados) return;
+		String base = "res/ZOMBIE/Zombie-Tileset---_0";
+		String suf = "_Capa-";
+		// 0430..0432 = down (frente); 0433..0435 = left (lado); 0436..0438 = up (costas)
+		for (int i = 0; i < 3; i++) {
+			sprDown[i]  = new ImageIcon(base + (430 + i) + suf + (431 + i) + ".png").getImage();
+			sprRight[i] = new ImageIcon(base + (433 + i) + suf + (434 + i) + ".png").getImage();
+			sprUp[i]    = new ImageIcon(base + (436 + i) + suf + (437 + i) + ".png").getImage();
+		}
+		String baseDmg = "res/ZOMBIE/ZOMBIE_DAMAGED/Zombie-Tileset---_0";
+		// 0439..0441 = down; 0442..0444 = left; 0445..0447 = up
+		for (int i = 0; i < 3; i++) {
+			sprDownDmg[i]  = new ImageIcon(baseDmg + (439 + i) + suf + (440 + i) + ".png").getImage();
+			sprRightDmg[i] = new ImageIcon(baseDmg + (442 + i) + suf + (443 + i) + ".png").getImage();
+			sprUpDmg[i]    = new ImageIcon(baseDmg + (445 + i) + suf + (446 + i) + ".png").getImage();
+		}
+		spritesCarregados = true;
 	}
 
 	public void atualizar(Player p, tileMap cena) {
@@ -44,22 +84,47 @@ public class Zumbi {
 		}
 		areaColisao.x = posX;
 		areaColisao.y = posY;
+
+		if (Math.abs(alvoY - posY) > Math.abs(alvoX - posX)) {
+			direcao = (dy < 0) ? "up" : "down";
+		} else if (dx != 0) {
+			direcao = "right";
+			// sprite base "right" eh na verdade left; espelhar quando movendo pra direita
+			olhandoEsquerda = (dx > 0);
+		}
+
+		contadorFrame++;
+		if (contadorFrame >= TICKS_POR_FRAME) {
+			frame = (frame + 1) % 3;
+			contadorFrame = 0;
+		}
+		if (tempoDano > 0) tempoDano--;
 	}
 
 	public void recebeDano() {
 		this.vida--;
+		this.tempoDano = DURACAO_DANO;
 		if (this.vida <= 0) this.vivo = false;
 	}
 
 	public void desenhar(Graphics2D d2) {
 		if (!vivo) return;
-		d2.setColor(new Color(40, 110, 40));
-		d2.fillRect(posX, posY, LARG, ALTU);
-		d2.setColor(new Color(20, 60, 20));
-		d2.drawRect(posX, posY, LARG, ALTU);
-		// olhos pra ficar menos abstrato
-		d2.setColor(Color.RED);
-		d2.fillOval(posX + 8, posY + 12, 6, 6);
-		d2.fillOval(posX + LARG - 14, posY + 12, 6, 6);
+		boolean dmg = tempoDano > 0;
+		Image img;
+		switch (direcao) {
+			case "up":    img = dmg ? sprUpDmg[frame]    : sprUp[frame];    break;
+			case "right": img = dmg ? sprRightDmg[frame] : sprRight[frame]; break;
+			default:      img = dmg ? sprDownDmg[frame]  : sprDown[frame];  break;
+		}
+		if (img == null) {
+			d2.setColor(new Color(40, 110, 40));
+			d2.fillRect(posX, posY, LARG, ALTU);
+			return;
+		}
+		if (olhandoEsquerda && direcao.equals("right")) {
+			d2.drawImage(img, posX + LARG, posY, -LARG, ALTU, null);
+		} else {
+			d2.drawImage(img, posX, posY, LARG, ALTU, null);
+		}
 	}
 }
